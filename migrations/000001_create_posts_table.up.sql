a74f4a73782ea1fcaeb9b7bb5bddc4350c4bc7c4
muhammadakbarra <13120210008@umi.ac.id>
2026-05-11 01:04:46 +0800

    fix: make migration script idempotent using IF NOT EXISTS

--- migrations/000001_create_posts_table.up.sql
+++ migrations/000001_create_posts_table.up.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-CREATE TABLE posts (
+CREATE TABLE IF NOT EXISTS posts (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   title TEXT NOT NULL,
   content TEXT NOT NULL,
